8882a3ee28a4f3d1895016476cfa7fdf9019115e
benhutchinson <ben.hutchinson.personal@gmail.com>
2015-01-07 17:18:56 +0000

    Trying to integrate JSON with Google

---
+++ data/data_converter copy.rb
@@ -0,0 +1,30 @@
+require 'CSV'
+require 'json'
+
+
+data = {}
+
+
+CSV.foreach('new2.csv') do |row|
+  if row[2] == ' Amount'
+
+  elsif data.keys.include?row[0]
+    data[row[0]] += row[2].to_i
+  else
+    data[row[0]] = row[2].to_i
+  end
+end
+
+jsn = []
+
+data.each do |key, value|
+  jsn << {:department =>key, :spend => value}
+end
+
+doc = []
+
+jsn.each do |hash|
+  doc << hash.to_json
+end
+
+puts doc.to_json
