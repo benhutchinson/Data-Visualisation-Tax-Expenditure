de08bbc581b87ad152b166e0871edc0d5ee61175
John <jjkiely@gmail.com>
2015-01-07 16:32:46 +0000

    removed heading line from JSON

---
+++ data/data_converter2.rb
@@ -0,0 +1,27 @@
+require 'CSV'
+require 'json'
+
+
+data = {}
+
+
+CSV.foreach('new2.csv') do |row|
+  if row[2] == 'Amount'
+
+  elsif data.keys.include?row[0]
+    data[row[0]] += row[2].to_i
+  else
+    data[row[0]] = row[2].to_i
+  end
+end
+
+jsn = []
+
+data.each do |key, value|
+  jsn << {:department =>key, :spend => value}
+end
+
+
+jsn.each do |hash|
+  puts hash.to_json
+end
