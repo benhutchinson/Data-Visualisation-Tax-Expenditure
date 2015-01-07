be48f651c2d9b104dd4b7cd1d805ccca89aab7e3
benhutchinson <ben.hutchinson.personal@gmail.com>
2015-01-07 12:14:52 +0000

    Sinatra App

---
+++ config.ru
@@ -0,0 +1,3 @@
+require_relative './server'
+
+Sinatra::Application.run!
---
+++ server.rb
@@ -0,0 +1,10 @@
+require 'sinatra'
+
+set :views, Proc.new { File.join(root, "/views") }
+set :public_folder, Proc.new { File.join(root, "/public") }
+
+get '/' do 
+  erb :index
+end
+
+
