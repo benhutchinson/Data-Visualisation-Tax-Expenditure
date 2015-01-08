a1ac7cd1820c30666aa153048ffd176f23e763bb
benhutchinson <ben.hutchinson.personal@gmail.com>
2015-01-08 12:01:13 +0000

    server links

--- server.rb
+++ server.rb
@@ -6,3 +6,23 @@ set :public_folder, Proc.new { File.join(root, "/public") }
 get '/' do
   erb :index
 end
+
+get '/nav' do 
+  erb :nav
+end
+
+get '/google_gauge' do 
+  erb :google_gauge
+end
+
+get '/google_pie' do 
+  erb :google_pie
+end
+
+get '/google_bar' do 
+  erb :google_bar
+end
+
+get '/d3' do 
+  erb :d3
+end
